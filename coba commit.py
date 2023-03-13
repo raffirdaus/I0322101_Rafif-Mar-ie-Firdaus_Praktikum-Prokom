9290fa73d23da1d1d63582d87bf27e649cf9e4f2
Rafif <127171479+raffirdaus@users.noreply.github.com>
2023-03-13 12:54:53 +0700

    Delete coba commit.py

--- coba commit.py
+++
@@ -1,7 +0,0 @@
-n=int (input("masukan angka"))
-for x in range(2,n):
-    if n%x == 0:
-        print(f"{n} adalah bukan bilangan prima")
-        break
-else:
-    print (f"{n} adalah bilangan prima")
