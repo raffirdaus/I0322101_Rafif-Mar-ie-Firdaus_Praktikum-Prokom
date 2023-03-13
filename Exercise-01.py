43c80461df3a0292c9228949840954839483d0c1
Rafif <127171479+raffirdaus@users.noreply.github.com>
2023-03-13 12:38:00 +0700

    Delete Exercise-01.py

--- Exercise-01.py
+++
@@ -1,8 +0,0 @@
-print("Rafif Mar'ie Firdaus")
-print("I0322101")
-
-print("------------------------------------")
-
-print("Halo, namaku Rafif Mar'ie Firdaus biasa dipanggil Rafif, aku dari Teknik Industri UNS dan nimku I0322101. Aku dari Yogyakarta, tapi sekarang lagi tinggal di Jaten, Karanganyar")
-print("Di rumahku yang di Yogya sangat tenang dikarenakan perumahanku terpisah dari jalan raya. Setiap pagi di sambut oleh kicauan burung dan matahari yang tidak terlalu terik dengan hawa yang tenang. Setiap pagi kamu bisa melihat residen - residen perumahan berolahraha di pagi hari baik itu jogging, jalan kaki, ataupun bersepeda. Selain itu, di perumahanku akan terasa panas saat sudah siang hari, matahari yang terik menusuk dengan hawa panas yang ditemani dengan angin yang terkadang panas terkadang sejuk. Panas tersebut sendiri mulai akan pudar mendekati sore hari dengan terasa kembali hawa yang tenang dan sejuk itu mendekati terbenamnya matahari di barat dengan awan dan langit menemaninya. Tidak langka melihat langit yang berwarna di kampung halamanku yang mengingatkan rasa kehangatan dengan kiasan kota istemawa menemani langit berwarna.")
-print("Setelah lulus dan mendapatkan gelar ST dari teknik industri saya berharap dapat bekerja di perusahaan - perusahaan yang berurusan dalam bidang otomotif ataupun pakaian. Hal ini dikarenakan keiginan saya saat melalui masa sma dimana saya terpana dengan impian bekerja untuk Adidas ataupun Toyota. Saya juga berharap dapat mengejar impian saya dan mengharumkan nama keluarga saya terutama untuk membanggakan kedua orang tua saya dan membawa nama baik keluarga saya. Saya juga berharap dapat membangkit keluarga sendiri dari kerja keras saya dan melepaskan beban dari kedua orang tua saya.")
